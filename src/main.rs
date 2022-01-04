8eda15ca93d6853ee29c853fb25b1b8820496432
Saetch <nils.wriedt@hotmail.com>
2022-01-04 12:40:26 +0100

    added basic file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use crate::controller::controller::Controller;
+
+pub mod model;
+pub mod view;
+pub mod controller;
 fn main() {
     println!("Hello, world! One day I will be a real program!");
+
+    let cntrl = Controller{};
 }
